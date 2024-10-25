2ac4b0340fb42a9855c64fbec9b6e650e36a412a
Srishti3108 <138809318+Srishti3108@users.noreply.github.com>
2024-10-25 18:28:32 +0530

    Add files via upload

---
+++ C#_Assignment/DailyAssignment/DailyAssignment/TransportSchedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyAssignment
+{
+    internal class TransportSchedule
+    {
+        public string TransportType { get; set; }   
+        public string Route { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public decimal Price { get; set; }
+        public int SeatsAvailable { get; set; }
+    }
+}
